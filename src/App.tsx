655bb641e3ecd6b230c3c803301086541c1faf7b
Julia Kieserman <jbk67@georgetown.edu>
2018-06-03 17:59:32 -0400

    add blueprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './App.css';
 
+import Patient from "./components/Patient";
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -11,9 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Patient />
       </div>
     );
   }
